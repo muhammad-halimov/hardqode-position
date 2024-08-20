b76e0f11eaec9a70b0258b1910159c6e203a10fe
muhammad-halimov <muhammadi.halimov@icloud.com>
2024-08-21 03:57:42 +0400

    Final

--- product/api/v1/permissions.py
+++ product/api/v1/permissions.py
@@ -1,26 +1,34 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
 from users.models import Subscription
-
-
-def make_payment(request):
-    # TODO
-    pass
 
 
 class IsStudentOrIsAdmin(BasePermission):
     def has_permission(self, request, view):
-        # TODO
-        pass
+        if request.user.is_staff:
+            return True
+        elif request.user.is_authenticated:
+            course_id = view.kwargs.get('id')
+            if course_id:
+                try:
+                    subscription = Subscription.objects.get(user=request.user, course=course_id, is_active=True)
+                    return True
+                except Subscription.DoesNotExist:
+                    return False
+        return False
 
     def has_object_permission(self, request, view, obj):
-        # TODO
-        pass
+        if request.user.is_staff:
+            return True
+        elif request.user.is_authenticated:
+            # Check if the user has an active subscription for the object's course
+            try:
+                subscription = Subscription.objects.get(user=request.user, course=obj.course.id, is_active=True)
+                return True
+            except Subscription.DoesNotExist:
+                return False
+        return False
 
 
 class ReadOnlyOrIsAdmin(BasePermission):
-
     def has_permission(self, request, view):
-        return request.user.is_staff or request.method in SAFE_METHODS
-
-    def has_object_permission(self, request, view, obj):
         return request.user.is_staff or request.method in SAFE_METHODS
